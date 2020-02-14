aff54397b1d63dc4fc8b9a2ee52c59980402754d
Laszlo Szucs <laszlo.szucs@bitrise.io>
2020-02-14 13:34:26 +0100

    remove input

--- main.go
+++ main.go
@@ -24,7 +24,6 @@ type Config struct {
 	GitlabAPIToken   stepconf.Secret `env:"gitlab_api_token"`
 	GitlabHost       string          `env:"gitlab_host"`
 	GitlabAPIBaseURL string          `env:"gitlab_api_base_url"`
-	FailOnErrors     string          `env:"fail_on_errors,opt[true,false]"`
 }
 
 func validateInputs(cfg Config) {
@@ -84,7 +83,6 @@ func main() {
 		"DANGER_GITLAB_API_TOKEN":    string(cfg.GitlabAPIToken),
 		"DANGER_GITLAB_HOST":         cfg.GitlabHost,
 		"DANGER_GITLAB_API_BASE_URL": cfg.GitlabAPIBaseURL,
-		"DANGER_FAIL_ON_ERRORS":      cfg.FailOnErrors,
 	} {
 		if value != "" {
 			if err := os.Setenv(key, value); err != nil {
@@ -140,8 +138,7 @@ func main() {
 	fmt.Println()
 	log.Infof("Running danger")
 
-	failOnErrorsFlag := fmt.Sprintf("--fail-on-errors=%s", cfg.FailOnErrors)
-	cmd = command.New("bundle", "exec", "danger", failOnErrorsFlag)
+	cmd = command.New("bundle", "exec", "danger")
 	cmd.SetStdout(os.Stdout)
 	cmd.SetStderr(os.Stderr)
 	log.Printf("$ %s", cmd.PrintableCommandArgs())
